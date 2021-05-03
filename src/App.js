c86decec0db35e9dfff947602c8d47660f3dbc16
upluke <lukeforadream@gmail.com>
2021-05-02 23:59:28 -0700

    timerPage

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
- 
+import TimerPage from './components/TimerPage'
 
 function App() {
   return (
     <div className="App">
         <h1>BeeTimer</h1>
         <h1>Test</h1>
+        <TimerPage/>
     </div>
   );
 }
---
+++ src/components/TimerPage.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const TimerPage = () => {
+    return <h1>TImer</h1>
+}
+
+export default TimerPage
